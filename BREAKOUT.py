3ed8cc5b652550581ba07bc7176b633701e04f45
Chrissy Wake UP, I don like this, CHRISSY wake up! <universecrusader16@gmail.com>
2025-02-12 14:50:25 -0700

    BREAKOUT.py

--- BREAKOUT.py
+++ BREAKOUT.py
@@ -23,48 +23,10 @@ class brick:
         self.color = (random.randrange(100, 250),random.randrange(100, 250),random.randrange(100, 250))
         self.isDead = False
     def draw(self):
-        pygame.draw.rect(screen, self.color, (self.xpos, self.ypos, 100, 50))
-    def draw(self):
         if not self.isDead:
             pygame.draw.rect(screen, self.color, (self.xpos, self.ypos, 100, 50)) # Width and height are 100 and 50
     #bounding box collision
     def collide(self, ball_x, ball_y):
-        if b1.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b2.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b3.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b4.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b5.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b6.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b7.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b8.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b9.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b10.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b11.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
-        if b12.collide(ball_x, ball_y):
-            bVy *= -1
-            p1Score += 1
         if not self.isDead:
             if (ball_x + ball_size > self.xpos and
                 ball_x < self.xpos + 100 and  # Width of brick is 100
@@ -73,7 +35,32 @@ class brick:
                 self.isDead = True
                 return True
         return False
-    
+    def collision(ball_x, ball_y):
+    # Ball collision with each brick
+        if b1.collide(ball_x, ball_y):
+            bvy *= -1
+        if b2.collide(ball_x, ball_y):
+            bvy *= -1
+        if b3.collide(ball_x, ball_y):
+            bvy *= -1
+        if b4.collide(ball_x, ball_y):
+            bvy *= -1
+        if b5.collide(ball_x, ball_y):
+            bvy *= -1
+        if b6.collide(ball_x, ball_y):
+            bvy *= -1
+        if b7.collide(ball_x, ball_y):
+            bvy *= -1
+        if b8.collide(ball_x, ball_y):
+            bvy *= -1
+        if b9.collide(ball_x, ball_y):
+            bvy *= -1
+        if b10.collide(ball_x, ball_y):
+            bvy *= -1
+        if b11.collide(ball_x, ball_y):
+            bvy *= -1
+        if b12.collide(ball_x, ball_y):
+            bvy *= -1
 
 # The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
@@ -101,24 +88,22 @@ while not doExit: #GAME LOOP####################################################
     #game logic will go here-------------------------------------
     bx += bVx
     by += bVy
-    
-    
 
     #reflect ball off side walls of screen
     if bx < 0: #hit left side 
         bVx *= -1
         
         
-    if bx > 700: #hit right side
+    if bx + 100 > 700: #hit right side
         bVx *= -1
         
     if by < 0 or by + 20 > 500:
         bVy *= -1
        
-        
+    
         
         #ball-paddle relfection
-    if bx < p1x + 50 and by + 0 > p1y and by < p1y + 0:
+    if bx < p1x + 50 and by + 20 > p1y and by < p1y + 100:
         bVx *= -1
 
     keys = pygame.key.get_pressed()
